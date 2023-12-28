c15d0cc1c2160c8cf1d52514d873f65e129e844a
이종경 <gyoung3063413@naver.com>
2023-12-28 16:28:56 +0900

    [리팩터링] 중복된 코드 함수화 및 DOM API 최소화

--- hacker-news/app.js
+++ hacker-news/app.js
@@ -4,35 +4,40 @@ const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-ajax.open('GET', NEWS_URL, false); // boolean => 동기/비동기 여부
-ajax.send();
+function getData(url) {
+    ajax.open('GET', url, false); // boolean => 동기/비동기 여부
+    ajax.send();
 
-const newsFeed = JSON.parse(ajax.response);
+    return JSON.parse(ajax.response);
+};
+
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
 window.addEventListener('hashchange', function () {
     const id = location.hash.substring(1)
 
-    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-    ajax.send();
-
-    const newsContent = JSON.parse(ajax.response);
+    const newsContent = getData(CONTENT_URL.replace('@id', id));
     const title = document.createElement('h1');
-    
+
     title.innerHTML = newsContent.title;
 
     content.appendChild(title);
-})
+});
 
 for (let i = 0; i < newsFeed.length; i++) {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
+    const div = document.createElement('div');
 
-    a.href = `#${newsFeed[i].id}`;
-    a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+    div.innerHTML = `
+    <li>
+        <a href="#${newsFeed[i].id}";">
+            ${newsFeed[i].title} (${newsFeed[i].comments_count})
+        </a>
+    </li>
+    `;
 
-    li.appendChild(a);
-    ul.appendChild(li);
+    // ul.appendChild(div.children[0]); // children은 배열을 반환
+    ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul);
