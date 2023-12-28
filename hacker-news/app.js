6bda47f13eddcb30e9524cc21d87f8f809789b4a
이종경 <gyoung3063413@naver.com>
2023-12-28 15:43:22 +0900

    [추가] XMLHttpRequest이용 AJAX요청 및 데이터 파싱

---
+++ hacker-news/app.js
@@ -0,0 +1,15 @@
+const ajax = new XMLHttpRequest();
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
+ajax.open('GET', NEWS_URL, false); // boolean => 동기/비동기 여부
+ajax.send();
+
+const newsFeed = JSON.parse(ajax.response);
+const ul = document.createElement('ul');
+
+for (let i = 0; i < newsFeed.length; i++) {
+    const li = document.createElement('li');
+    li.innerHTML = newsFeed[i].title
+    ul.appendChild(li);
+}
+
+document.getElementById('root').appendChild(ul);
