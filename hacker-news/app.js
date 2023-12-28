e4a0f5408dcd665399b0433f49894374e679510a
이종경 <gyoung3063413@naver.com>
2023-12-28 16:53:33 +0900

    [리팩터링] 함수 분리 및 라우터 생성

--- hacker-news/app.js
+++ hacker-news/app.js
@@ -11,34 +11,50 @@ function getData(url) {
     return JSON.parse(ajax.response);
 };
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
+function newsFeed() {
+    const newsFeed = getData(NEWS_URL);
+    const newsList = [];
 
-window.addEventListener('hashchange', function () {
+    newsList.push('<ul>');
+
+    for (let i = 0; i < newsFeed.length; i++) {
+        newsList.push(`
+        <li>
+            <a href="#${newsFeed[i].id}">
+                ${newsFeed[i].title} (${newsFeed[i].comments_count})
+            </a>
+        </li>
+        `);
+    }
+
+    newsList.push('</ul>');
+
+    container.innerHTML = newsList.join('');
+}
+
+function newsDetail() {
     const id = location.hash.substring(1)
 
     const newsContent = getData(CONTENT_URL.replace('@id', id));
-    const title = document.createElement('h1');
 
-    title.innerHTML = newsContent.title;
-
-    content.appendChild(title);
-});
-
-for (let i = 0; i < newsFeed.length; i++) {
-    const div = document.createElement('div');
-
-    div.innerHTML = `
-    <li>
-        <a href="#${newsFeed[i].id}";">
-            ${newsFeed[i].title} (${newsFeed[i].comments_count})
-        </a>
-    </li>
+    container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+    <div>
+    <a href="#">목록으로</a>
+    </div>
     `;
-
-    // ul.appendChild(div.children[0]); // children은 배열을 반환
-    ul.appendChild(div.firstElementChild);
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function router() {
+    const routePath = location.hash;
+
+    if (routePath === '') { // location.hash에 # 만 들어있을 경우엔 빈값을 반환한다.
+        newsFeed();
+    } else {
+        newsDetail();
+    }
+}
+
+window.addEventListener('hashchange', router);
+
+router();
