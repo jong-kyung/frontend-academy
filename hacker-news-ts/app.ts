8e1702077a7f72d500362049102909a5371e61a1
이종경 <gyoung3063413@naver.com>
2023-12-29 15:11:33 +0900

    [수정] typealias -> interface 변경

--- hacker-news-ts/app.ts
+++ hacker-news-ts/app.ts
@@ -1,30 +1,30 @@
-type Store = {
+interface Store { // 유니온타입은 인터페이스를 통해 나타낼 수 없음. 따라서 type alias로 작성해주어야함. 보통 타입을 정의할때, 유니온타입이 없으면 인터페이스를 사용함
     currentPage: number;
     feeds: NewsFeed[];
 }
 
-type News = {
-    id: number;
-    time_ago: string;
-    title: string;
-    url: string;
-    user: string;
-    content: string
+interface News {
+    readonly id: number; // readonly 지시어를 typealias혹은 인터페이스로 정의해준 타입앞에 작성해주면 수정이 불가능함.
+    readonly time_ago: string;
+    readonly title: string;
+    readonly url: string;
+    readonly user: string;
+    readonly content: string
 }
 
-type NewsFeed = News & { // type alias의 기능으로 합칠 수 있음
-    comments_count: number;
-    points: number;
+interface NewsFeed extends News { // extends를 통해 확장함.
+    readonly comments_count: number;
+    readonly points: number;
     read?: boolean; // optional 타입설정
 }
 
-type NewsDetail = News & {
-    comments: NewsComment[];
+interface NewsDetail extends News {
+    readonly comments: NewsComment[];
 }
 
-type NewsComment = News & {
-    comments: NewsComment[];
-    level: number;
+interface NewsComment extends News {
+    readonly comments: NewsComment[];
+    readonly level: number;
 }
 
 const container: HTMLElement | null = document.getElementById('root');
